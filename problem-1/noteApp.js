5eded2d22d46400a7c26a78bda71779c9bd33b93
Jina Park <jinapark119@gmail.com>
2022-11-17 00:50:22 +0000

    Convert every button function to rxjs

--- problem-1/noteApp.js
+++ problem-1/noteApp.js
@@ -1,8 +1,8 @@
 // import { Observable } from 'rxjs'; 
-import { fromEvent } from 'rxjs'; // Import the fromEvent operator
+//import { fromEvent } from 'rxjs'; // Import the fromEvent operator
 
 const add_button = document.getElementById('add_note'); // Get the element with the specified id, "add_note"
-const btnObservable = fromEvent(add_button, 'click'); // Create an observable of button clicks 
+//const btnObservable = fromEvent(add_button, 'click'); // Create an observable of button clicks 
 
 // Retrieve the data in the local storage, retrieving "notes" object here
 // It finds the data somewhere and creates the JSON value to put into const variable
@@ -19,14 +19,10 @@ if (notePads) {
     }); 
 } // end if
 
-// // When the add button is clicked then call the function addNote()
-// Rx.Observable.fromEvent(add_button, 'click').subscribe(() => { 
-//     addNote(); 
-//     console.log('Clicked!');
-// }); 
-
-btnObservable.fromEvent(add_button, 'click').subscribe(() => { 
+// When the add button is clicked then call the function addNote()
+Rx.Observable.fromEvent(add_button, 'click').subscribe(() => { 
     addNote(); 
+    //console.log('Clicked!');
 }); 
 
 // Function which adds notes
@@ -67,13 +63,13 @@ function addNote(text = "") {
     textArea.value = text;
     main.innerHTML = marked(text);
 
-    edit_button.addEventListener("click", () => {
+    Rx.Observable.fromEvent(edit_button, 'click').subscribe(() => {
         main.classList.toggle("hidden");
         textArea.classList.toggle("hidden");
     });
 
     // When the delete button is clicked then remove the note and update to local storage
-    del_button.addEventListener("click", () => {
+    Rx.Observable.fromEvent(del_button, 'click').subscribe(() => {
         note.remove();
         uploadToLS();
     });
