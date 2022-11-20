f84ff9fc3911453d57197d0fa73b237ec75260ed
Jina Park <jinapark119@gmail.com>
2022-11-20 00:31:33 +0000

    Decorate the timer

---
+++ problem-2/timer.js
@@ -0,0 +1,12 @@
+// import { timer } from 'rxjs'; 
+// import { fromEvent } from 'rxjs'; // Import the fromEvent operator
+
+// const source = Rx.Observable.timer(1000);
+// const subscribe = source.subscribe(value => console.log(value));
+
+const count = 10;
+// Rx.Observable
+//     .timer(100, 100)
+//     .map(i => count - i)
+//     .take(count + 1)
+//     .subscribe(i => console.log(i));
