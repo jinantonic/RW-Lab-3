6ff80ac2832603c5cfee6f2ab971bde3a09d45a7
Jina Park <jinapark119@gmail.com>
2022-11-21 00:30:32 +0000

    Create variables and functions for the timer

--- problem-2/timer.js
+++ problem-2/timer.js
@@ -1,10 +1,39 @@
-// import { timer } from 'rxjs'; 
-// import { fromEvent } from 'rxjs'; // Import the fromEvent operator
+const hrInput = document.getElementById('hr');
+const minInput = document.getElementById('min');
+const secInput = document.getElementById('sec');
 
-// const source = Rx.Observable.timer(1000);
-// const subscribe = source.subscribe(value => console.log(value));
+// Instantiate hour, minute, and second to 0
+let hr = 0;
+let min = 0;
+let sec = 0;
 
-const count = 10;
+// Instantiate timer to 0
+let timer = 0;
+
+const updateTime = () => {
+    hrInput.value = hr;
+    minInput.value = min;
+    secInput.value = sec;
+}
+
+Rx.Observable.fromEvent(hrInput, 'change').subscribe(() => {
+    hr = parseInt(hrInput.value);
+    updateTime();
+})
+
+Rx.Observable.fromEvent(minInput, 'change').subscribe(() => {
+    min = parseInt(minInput.value);
+    updateTime();
+})
+
+Rx.Observable.fromEvent(secInput, 'change').subscribe(() => {
+    sec = parseInt(secInput.value);
+    updateTime();
+})
+
+
+
+// const count = 10;
 // Rx.Observable
 //     .timer(100, 100)
 //     .map(i => count - i)
